8dbbb0661738cedb93dc65ff4c79a7af427de0d8
Danila Drozdov <drozdovdanilsss@gmail.com>
2024-10-22 18:40:18 +0800

    Update endpoint.cpp

--- src/endpoint.cpp
+++ src/endpoint.cpp
@@ -73,6 +73,7 @@ const ConfFile::OptionsTable UartEndpoint::option_table[] = {
     {"BlockSrcCompIn",  false, ConfFile::parse_uint8_vector,    OPTIONS_TABLE_STRUCT_FIELD(UartEndpointConfig, block_src_comp_in)},
     {"AllowSrcSysIn",   false, ConfFile::parse_uint8_vector,    OPTIONS_TABLE_STRUCT_FIELD(UartEndpointConfig, allow_src_sys_in)},
     {"BlockSrcSysIn",   false, ConfFile::parse_uint8_vector,    OPTIONS_TABLE_STRUCT_FIELD(UartEndpointConfig, block_src_sys_in)},
+    {"LimitAttitudeRate", false, ConfFile::parse_bool, OPTIONS_TABLE_STRUCT_FIELD(UdpEndpointConfig, limit_attitude_rate)},
     {"group",           false, ConfFile::parse_stdstring,       OPTIONS_TABLE_STRUCT_FIELD(UartEndpointConfig, group)},
     {}
 };
