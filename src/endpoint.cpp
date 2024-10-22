e00fda3cbb5cd901c4eceef0af04be9d2894a790
Danila Drozdov <drozdovdanilsss@gmail.com>
2024-10-22 19:37:10 +0800

    Update endpoint.cpp

--- src/endpoint.cpp
+++ src/endpoint.cpp
@@ -1386,6 +1386,7 @@ ssize_t UdpEndpoint::_read_msg(uint8_t *buf, size_t len)
 int UdpEndpoint::write_msg(const struct buffer *pbuf)
 {
     // Check if the message can be sent based on rate limits
+	log_info(this->config.limit_attitude_rate);
 	if (this->config.limit_attitude_rate) {
 	    if (pbuf->curr.msg_id == 30 && !can_send_msg(pbuf->curr.msg_id)) {
 	        //log_info("UDP %s: Rate limit exceeded for msg_id %u", _name.c_str(), pbuf->curr.msg_id);
